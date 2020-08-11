50d11afe2dceb874056328861d8055aa9a3af3db
Mikail Masood <50792378+mikail-masood@users.noreply.github.com>
2020-08-11 16:09:55 -0400

    hid secrets

--- config_bot_v1.py
+++ config_bot_v1.py
@@ -1,4 +1,4 @@
-username = "caramel_mocha"
-password = "1Forrest1!"
-client_id = "8a4wTXbkq4286w"
-client_secret = "ZYKpF59s06vuiOes3Z_Fjfsueyo"
+username = "username"
+password = "password"
+client_id = "id key"
+client_secret = "secret key"
