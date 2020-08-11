ff972b989c77a11be8590a6b75156b57df13bd84
Mikail Masood <50792378+mikail-masood@users.noreply.github.com>
2020-08-11 16:10:39 -0400

    hid secrets

--- config_dog.py
+++ config_dog.py
@@ -1,5 +1,5 @@
 # config
-username = "caramel_mocha"
-password = "1Forrest1!"
-client_id = "8a4wTXbkq4286w"
-client_secret = "ZYKpF59s06vuiOes3Z_Fjfsueyo"
+username = "username"
+password = "password"
+client_id = "id key"
+client_secret = "secret key"
